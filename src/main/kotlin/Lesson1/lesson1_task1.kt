9b3945d080138e2d29b5f3aaef989c1fe42c6234
Valery <i@vvaganov.ru>
2024-05-16 19:47:44 +0300

    KS-1-1 variables name

---
+++ src/main/kotlin/Lesson1/lesson1_task1.kt
@@ -0,0 +1,9 @@
+package Lesson1
+
+fun main(){
+    val numberOfOrders: Int = 75
+    val messageToTheClient: String = "Thank you for your purchase"
+
+    println(numberOfOrders)
+    println(messageToTheClient)
+}
