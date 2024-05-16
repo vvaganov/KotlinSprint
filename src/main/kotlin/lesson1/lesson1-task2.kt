21012adeccf688b35e6a82a0117ed06d97aff202
Valery <i@vvaganov.ru>
2024-05-16 22:54:24 +0300

    KS-1-2 adding and changing a variable var

---
+++ src/main/kotlin/lesson1/lesson1-task2.kt
@@ -0,0 +1,17 @@
+package lesson1
+
+fun main(){
+    val numberOfOrders: Int = 75
+    val messageToTheClient: String = "Thank you for your purchase"
+
+    println(numberOfOrders)
+    println(messageToTheClient)
+
+    var numberOfEmployees: Int = 2000
+
+    //println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+
+    println(numberOfEmployees)
+}
