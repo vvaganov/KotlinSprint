24cb59cd8749668f07ba03b75122881e4e1b8163
Valery <i@vvaganov.ru>
2024-05-18 22:32:52 +0300

    KS-2-1 arithmetic operations

---
+++ src/main/kotlin/lesson2/lesson2-task1.kt
@@ -0,0 +1,14 @@
+package lesson2
+
+fun main(){
+    val numberOfStudents = 4
+
+    val evaluation1: Double = 3.0
+    val evaluation2: Double = 4.0
+    val evaluation3: Double = 3.0
+    val evaluation4: Double = 5.0
+
+    val averageScore: Double = (evaluation1+evaluation2+evaluation3+evaluation4)/numberOfStudents
+
+    println(averageScore)
+}
