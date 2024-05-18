ff312a1437d1c7bdf24aa719a2e40e07cbbb1e02
Valery <i@vvaganov.ru>
2024-05-18 17:17:35 +0300

    KS-1-4 Numeric data types and printing

---
+++ src/main/kotlin/lesson1-task4.kt
@@ -0,0 +1,17 @@
+package lesson1
+
+fun main(){
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075F
+    val second: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(second)
+    println(partOfYear)
+    println(apogee)
+}
