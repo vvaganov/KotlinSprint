ae8996ca31c11f806ef9a26f714107c609d31193
Valery <i@vvaganov.ru>
2024-05-18 21:36:27 +0300

    KS-1-4 Numeric data types and printing

---
+++ src/main/kotlin/lesson1/lesson1-task5.kt
@@ -0,0 +1,18 @@
+package lesson1
+
+import kotlin.math.min
+
+fun main(){
+
+    val second = 6480
+
+    val hour = second/3600
+    val minute = (second/60 )% 60
+    val seconds = (second % minute)
+
+    println(hour)
+    println(minute)
+    println(seconds)
+
+    println("0$hour:$minute:0$seconds")
+}
