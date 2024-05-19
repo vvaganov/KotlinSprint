1b5712dfaf1144b65864df4e56cb5a3d30d739dc
Valery <i@vvaganov.ru>
2024-05-19 10:49:25 +0300

    KS-2-1 Ctrl+Alt+L auto format

--- src/main/kotlin/lesson2/lesson2-task1.kt
+++ src/main/kotlin/lesson2/lesson2-task1.kt
@@ -1,6 +1,6 @@
 package lesson2
 
-fun main(){
+fun main() {
     val numberOfStudents = 4
 
     val evaluation1: Double = 3.0
@@ -8,7 +8,7 @@ fun main(){
     val evaluation3: Double = 3.0
     val evaluation4: Double = 5.0
 
-    val averageScore: Double = (evaluation1+evaluation2+evaluation3+evaluation4)/numberOfStudents
+    val averageScore: Double = (evaluation1 + evaluation2 + evaluation3 + evaluation4) / numberOfStudents
 
     println(averageScore)
 }
