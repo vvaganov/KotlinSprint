db54c50af75c02a72a8c7bdbb88399e7b80439d3
Valery <i@vvaganov.ru>
2024-05-20 08:45:33 +0300

    KS-2-3 constants in uppercase, an empty string after declaring variables

--- src/main/kotlin/lesson2/lesson2-task4.kt
+++ src/main/kotlin/lesson2/lesson2-task4.kt
@@ -1,11 +1,11 @@
 package lesson2
 
-const val percentCoefficient = 100
+const val PERCENT_COEFFICIENT = 100
 
 fun main() {
 
     val unitBaf = 20.0
-    val unitBafInPercent = unitBaf / percentCoefficient
+    val unitBafInPercent = unitBaf / PERCENT_COEFFICIENT
     val crystalOre = 7.0
     val ironOre = 11.0
     val cristalOreBaf: Double = crystalOre * unitBafInPercent
