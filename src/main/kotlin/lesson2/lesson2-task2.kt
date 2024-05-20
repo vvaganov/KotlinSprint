5da4b016c26da53e4a517688bd9c799653b93d73
Valery <i@vvaganov.ru>
2024-05-20 08:59:27 +0300

    KS-2-2-an empty string after declaring variables

--- src/main/kotlin/lesson2/lesson2-task2.kt
+++ src/main/kotlin/lesson2/lesson2-task2.kt
@@ -2,13 +2,21 @@ package lesson2
 
 fun main() {
     val numberOfEmployees = 50
+
     val salaryOfEmployees = 30000
+
     val numberOfInterns = 30
+
     val salaryOfInterns = 20000
+
     val theWageFundEmployees = numberOfEmployees * salaryOfEmployees
+
     val theWageFundInterns = numberOfInterns * salaryOfInterns
+
     val theGeneralSalaryFund = theWageFundInterns + theWageFundEmployees
+
     val totalNumberOfEmployees = numberOfEmployees + numberOfInterns
+
     val averageSalary = theGeneralSalaryFund / totalNumberOfEmployees
 
     println(theWageFundEmployees)
