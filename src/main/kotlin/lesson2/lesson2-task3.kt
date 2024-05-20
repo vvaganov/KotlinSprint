51707dbef325b35d3ce2d228f63cd918fc295842
Valery <i@vvaganov.ru>
2024-05-20 08:43:16 +0300

    KS-2-3 constants in uppercase, an empty string after declaring variables

--- src/main/kotlin/lesson2/lesson2-task3.kt
+++ src/main/kotlin/lesson2/lesson2-task3.kt
@@ -1,16 +1,24 @@
 package lesson2
 
-const val minuteInHour = 60
+const val MINUTE_IN_HOUR = 60
 
 fun main() {
     val theHourOfDepartureTime = 9
+
     val minutesOfDepartureTime = 36
+
     val travelTimeInMinutes = 457
-    val travelTimeInHour = travelTimeInMinutes / minuteInHour
-    val remainingMinutes = travelTimeInMinutes % minuteInHour
+
+    val travelTimeInHour = travelTimeInMinutes / MINUTE_IN_HOUR
+
+    val remainingMinutes = travelTimeInMinutes % MINUTE_IN_HOUR
+
     val arrivalTimeInMinutes = minutesOfDepartureTime + remainingMinutes
-    val arrivalTimeHour = (theHourOfDepartureTime + travelTimeInHour) + arrivalTimeInMinutes / minuteInHour
-    val arrivalTimeMinutes = arrivalTimeInMinutes % minuteInHour
+
+    val arrivalTimeHour = (theHourOfDepartureTime + travelTimeInHour) + arrivalTimeInMinutes / MINUTE_IN_HOUR
+
+    val arrivalTimeMinutes = arrivalTimeInMinutes % MINUTE_IN_HOUR
+
     val arrivalTime = String.format("%02d:%02d", arrivalTimeHour, arrivalTimeMinutes)
 
     println(arrivalTime)
