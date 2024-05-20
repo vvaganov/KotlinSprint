7da7ec4f85e484e83f71e519f1365cde3d937b9b
Valery <i@vvaganov.ru>
2024-05-20 08:34:20 +0300

    KS-1-5 constants in upper case, auto format

--- src/main/kotlin/lesson1/lesson1-task5.kt
+++ src/main/kotlin/lesson1/lesson1-task5.kt
@@ -2,19 +2,19 @@ package lesson1
 
 import kotlin.math.min
 
-const val secondInMinute = 60
-const val minuteInHour = 60
+const val SECOND_IN_MINUTE = 60
+const val MINUTE_IN_HOUR = 60
 
-fun main(){
+fun main() {
 
     val secondInSpace: Int = 6480
-    val secondInHour = secondInMinute * minuteInHour
+    val secondInHour = SECOND_IN_MINUTE * MINUTE_IN_HOUR
 
-    val hour = secondInSpace/secondInHour
-    val minute = (secondInSpace/secondInMinute) % minuteInHour
-    val seconds = (secondInSpace % secondInMinute)
+    val hour = secondInSpace / secondInHour
+    val minute = (secondInSpace / SECOND_IN_MINUTE) % MINUTE_IN_HOUR
+    val seconds = (secondInSpace % SECOND_IN_MINUTE)
 
-    val timesInCosmos = String.format("%02d:%02d:%02d", hour, minute,seconds)
+    val timesInCosmos = String.format("%02d:%02d:%02d", hour, minute, seconds)
 
     println(timesInCosmos)
 }
