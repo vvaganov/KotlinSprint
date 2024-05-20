a32e55e14167c264019e6e1825ec18e4729b9616
Valery <i@vvaganov.ru>
2024-05-20 09:18:49 +0300

    KS-2-3 an empty line after declaring variables

--- src/main/kotlin/lesson2/lesson2-task3.kt
+++ src/main/kotlin/lesson2/lesson2-task3.kt
@@ -4,21 +4,14 @@ const val MINUTE_IN_HOUR = 60
 
 fun main() {
     val theHourOfDepartureTime = 9
-
     val minutesOfDepartureTime = 36
-
     val travelTimeInMinutes = 457
 
     val travelTimeInHour = travelTimeInMinutes / MINUTE_IN_HOUR
-
     val remainingMinutes = travelTimeInMinutes % MINUTE_IN_HOUR
-
     val arrivalTimeInMinutes = minutesOfDepartureTime + remainingMinutes
-
     val arrivalTimeHour = (theHourOfDepartureTime + travelTimeInHour) + arrivalTimeInMinutes / MINUTE_IN_HOUR
-
     val arrivalTimeMinutes = arrivalTimeInMinutes % MINUTE_IN_HOUR
-
     val arrivalTime = String.format("%02d:%02d", arrivalTimeHour, arrivalTimeMinutes)
 
     println(arrivalTime)
