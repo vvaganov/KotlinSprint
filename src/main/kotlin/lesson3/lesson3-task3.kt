62d90b0bf83aec005e6f52f6566b243e68536752
Valery <i@vvaganov.ru>
2024-05-20 15:01:57 +0300

    KS-3-3 The multiplication table

---
+++ src/main/kotlin/lesson3/lesson3-task3.kt
@@ -0,0 +1,12 @@
+package lesson3
+
+fun main() {
+    val number = 8
+
+    val result = "${number*1}\n${number*2}\n${number * 3}\n" +
+            "${number * 4}\n${number * 5}\n" +
+            "${number * 6}\n${number * 7}\n" +
+            "${number * 8}\n${number * 9}"
+
+    println(result)
+}
