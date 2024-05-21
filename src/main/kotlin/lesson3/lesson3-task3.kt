90d994408bddc83e39732b0d4962dd29cc4ddff8
Valery <i@vvaganov.ru>
2024-05-21 10:11:07 +0300

    KS-3-3 Multiline interpolation + trimindent

--- src/main/kotlin/lesson3/lesson3-task3.kt
+++ src/main/kotlin/lesson3/lesson3-task3.kt
@@ -3,10 +3,17 @@ package lesson3
 fun main() {
     val number = 8
 
-    val result = "${number*1}\n${number*2}\n${number * 3}\n" +
-            "${number * 4}\n${number * 5}\n" +
-            "${number * 6}\n${number * 7}\n" +
-            "${number * 8}\n${number * 9}"
+    val result = """
+        $number * 1 = ${number * 1}
+        $number * 2 = ${number * 2}
+        $number * 3 = ${number * 3}
+        $number * 4 = ${number * 4}
+        $number * 5 = ${number * 5}
+        $number * 6 = ${number * 6}
+        $number * 7 = ${number * 7}
+        $number * 8 = ${number * 8}
+        $number * 9 = ${number * 9}
+        """.trimIndent()
 
     println(result)
 }
