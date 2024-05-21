1c0e6f7a54f5521f507549bf18532133909fce2c
Valery <i@vvaganov.ru>
2024-05-21 23:44:23 +0300

    KS-4-2 comparison operators + functions

--- src/main/kotlin/lesson4/lesson4-task2.kt
+++ src/main/kotlin/lesson4/lesson4-task2.kt
@@ -4,21 +4,24 @@ const val AVERAGE_WEIGHT_MIN = 35
 const val AVERAGE_WEIGHT_MAX = 100
 const val AVERAGE_WEIGHT_VOLUME = 100
 
-
 fun main() {
 
-    val cargoOneWeight = 20
-    val cargoOneVolume = 80
-    val cargoTwoWeight = 50
-    val cargoTwoVolume = 100
+    var cargoWeight = 20
+    var cargoVolume = 80
 
-    println(
-        "Груз весом $cargoOneWeight кг. и объемом $cargoOneVolume л. не соответствует категории \"Average\": " +
-                "${(cargoOneWeight >= AVERAGE_WEIGHT_MIN && cargoOneWeight <= AVERAGE_WEIGHT_MAX) && cargoOneVolume < AVERAGE_WEIGHT_VOLUME}"
-    )
 
-    println(
-        "Груз весом $cargoTwoWeight кг. и объемом $cargoTwoVolume л. не соответствует категории \"Average\": " +
-                "${(cargoTwoVolume >= AVERAGE_WEIGHT_MIN && cargoTwoWeight <= AVERAGE_WEIGHT_MAX) && cargoTwoVolume < AVERAGE_WEIGHT_VOLUME}"
-    )
+    fun compareCargoParameters(weight: Int, volume: Int) {
+        println(
+            "Груз весом $weight кг. и объемом $volume л. не соответствует категории \"Average\": " +
+                    "${(cargoWeight >= AVERAGE_WEIGHT_MIN && cargoWeight <= AVERAGE_WEIGHT_MAX) && cargoVolume < AVERAGE_WEIGHT_VOLUME}"
+        )
+    }
+
+    compareCargoParameters(cargoWeight, cargoVolume)
+
+    cargoWeight = 50
+    cargoVolume = 100
+
+    compareCargoParameters(cargoWeight, cargoVolume)
+
 }
