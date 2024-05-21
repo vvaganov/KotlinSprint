f0cfbd9b864208ab5cc500cfd9296847f42c3293
Valery <i@vvaganov.ru>
2024-05-21 10:40:18 +0300

    KS-3-3 replaced the numbers in the code with a variable

--- src/main/kotlin/lesson3/lesson3-task3.kt
+++ src/main/kotlin/lesson3/lesson3-task3.kt
@@ -2,17 +2,18 @@ package lesson3
 
 fun main() {
     val number = 8
+    var counter = 1
 
     val result = """
-        $number * 1 = ${number * 1}
-        $number * 2 = ${number * 2}
-        $number * 3 = ${number * 3}
-        $number * 4 = ${number * 4}
-        $number * 5 = ${number * 5}
-        $number * 6 = ${number * 6}
-        $number * 7 = ${number * 7}
-        $number * 8 = ${number * 8}
-        $number * 9 = ${number * 9}
+        $number * $counter = ${number * counter++}
+        $number * $counter = ${number * counter++}
+        $number * $counter = ${number * counter++}
+        $number * $counter = ${number * counter++}
+        $number * $counter = ${number * counter++}
+        $number * $counter = ${number * counter++}
+        $number * $counter = ${number * counter++}
+        $number * $counter = ${number * counter++}
+        $number * $counter = ${number * counter}
         """.trimIndent()
 
     println(result)
