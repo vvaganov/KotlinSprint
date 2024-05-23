685a6a50555d71aa5f6bba223a87855104da1695
Valery <i@vvaganov.ru>
2024-05-23 08:21:21 +0300

    KS-5-1

--- src/main/kotlin/lesson5/lessob5-task1.kt
+++ src/main/kotlin/lesson5/lessob5-task1.kt
@@ -1,11 +1,14 @@
 package lesson5
 
 fun main() {
-    println("Для авторизации, решите математическую задачку: 2 + 2 = ")
-    val answer = readln()
+    val number1 = 2
+    val number2 = 2
+    val sum = number1 + number2
+    println("Для авторизации, решите математическую задачку: $number1 + $number2 = ")
+    val answer = readln().toInt()
 
     when (answer) {
-        "4" -> println("Добро пожаловать!")
+        sum -> println("Добро пожаловать!")
         else -> println("Доступ запрещен")
     }
 
