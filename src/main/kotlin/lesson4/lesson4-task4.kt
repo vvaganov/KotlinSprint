eb58698018dbe03d996f22edb22e33afd7b5c6dc
Valery <i@vvaganov.ru>
2024-05-25 07:31:10 +0300

    KS-4-4

--- src/main/kotlin/lesson4/lesson4-task4.kt
+++ src/main/kotlin/lesson4/lesson4-task4.kt
@@ -2,17 +2,17 @@ package org.example.lesson4
 
 fun main() {
 
-    val trainingDay = 5
+    val trainingDay = 2
     val checkTrainingDay = trainingDay % 2
-    val groupOne = checkTrainingDay == 1
-    val groupTwo = checkTrainingDay != 1
+    val gisEven = checkTrainingDay == 0
+
 
     println(
         """
-       Упражнения для рук:   $groupOne
-       Упражнения для ног:   $groupTwo
-       Упражнения для спины: $groupTwo
-       Упражнения для пресса:$groupOne
+       Упражнения для рук:   ${!gisEven}
+       Упражнения для ног:   $gisEven
+       Упражнения для спины: $gisEven
+       Упражнения для пресса:${!gisEven}
    """.trimIndent()
     )
 
