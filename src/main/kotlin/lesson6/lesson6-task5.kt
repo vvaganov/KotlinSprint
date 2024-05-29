9cb7e32190e19c4354ad26669f65c9da6de74983
Valery <i@vvaganov.ru>
2024-05-29 08:34:59 +0300

    KS 6-5 authorization with a bot check v.2.

--- src/main/kotlin/lesson6/lesson6-task5.kt
+++ src/main/kotlin/lesson6/lesson6-task5.kt
@@ -2,30 +2,27 @@ package lesson6
 
 import kotlin.random.Random
 
-const val STOP = 0
-
 fun main() {
 
     var numberOfAttempts = 3
+    val minAttempts = 0
+
     println("Что бы доказать что Вы не бот решите следующую задачку:")
 
-    while (numberOfAttempts > STOP) {
+    while (numberOfAttempts > minAttempts) {
         val random1 = Random.nextInt(1, 9)
         val random2 = Random.nextInt(1, 9)
-        val random = random1 + random2
+        val amount = random1 + random2
         println("$random1 + $random2 = ")
         numberOfAttempts--
-        val testIn = readln().toInt()
-        if (testIn != random && numberOfAttempts == STOP) {
-            println("У Вас кончились попытки")
-            continue
-        } else if (testIn != random) {
-            println("Не праильно, поробуйте еще раз")
+        val result = readln().toInt()
+        if (result != amount) {
+            println("Не праильно!")
         } else {
             println("Ответ верный! Добро пожаловать.")
             return
         }
     }
-    println("Доступ запрещен!")
+    println("У Вас кончились попытки, доступ запрещен")
 }
 
