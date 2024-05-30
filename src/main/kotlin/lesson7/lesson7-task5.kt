76d6120a46a502271e4b91ddd8e5a18739138922
Valery <i@vvaganov.ru>
2024-05-30 11:28:29 +0300

    KS 7-5 v.2. initializing a variable after a loop

--- src/main/kotlin/lesson7/lesson7-task5.kt
+++ src/main/kotlin/lesson7/lesson7-task5.kt
@@ -3,13 +3,16 @@ import kotlin.random.Random
 fun main() {
 
     val minPasswordLength = 6
+    var listChars: List<Char>
+    var password: String
+    var passwordLength: Int
 
     do {
         println("Задайте количество символов в пароле:")
-        val passwordLength = readln().toInt()
+        passwordLength = readln().toInt()
         if (passwordLength >= minPasswordLength) {
-            val listChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
-            val password = (1..passwordLength).map { listChars.random() }.joinToString("")
+            listChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+            password = (1..passwordLength).map { listChars.random() }.joinToString("")
             println(password)
         } else {
             println("Символов должно быть не меньше $minPasswordLength")
