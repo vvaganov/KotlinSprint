08f4ce13ad6b261562471085d2dba4762b9ca8b0
Valery <i@vvaganov.ru>
2024-05-30 10:58:46 +0300

    KS 7-5 random password v.2. initializing variables outside the loop

--- src/main/kotlin/lesson7/lesson7-task2.kt
+++ src/main/kotlin/lesson7/lesson7-task2.kt
@@ -4,12 +4,14 @@ import kotlin.random.Random
 import kotlin.random.nextInt
 
 fun main() {
+    var verificationCode: Int
+    var randomCode: Int
 
     do {
-        val randomCode = Random.nextInt(1000..9999)
+        randomCode = Random.nextInt(1000..9999)
         println("Ваш код авторизации: $randomCode")
         println("Введите код:")
-        val verificationCode = readln().toInt()
+        verificationCode = readln().toInt()
     } while (randomCode != verificationCode)
     println("Добро пожаловать!")
 }
