c6b4fc54a4a0f931fd8be4c3a6a7d7128a78e490
Valery <i@vvaganov.ru>
2024-05-31 13:58:02 +0300

    KS 9-2 v.1. We calculated the number of ingredients from the number of servings set by the user

---
+++ src/main/kotlin/lesson9/lesson9-task3.kt
@@ -0,0 +1,13 @@
+package lesson9
+
+fun main() {
+    val oneServing = mapOf("яиц" to 2, "молока" to 50, "масла" to 15)
+    println("Введите количесто порций")
+    val numberOfServings = readln().toInt()
+    val ingredientsList = oneServing.mapValues { it.value * numberOfServings }
+
+    println("На количество порций $numberOfServings, Вам понадобится:")
+    for (i in ingredientsList) {
+        println("${i.key} - ${i.value}")
+    }
+}
