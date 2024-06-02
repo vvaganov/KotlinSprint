7647bca9c5a86d6cec69ae322198b057a76778de
Valery <i@vvaganov.ru>
2024-06-02 18:37:09 +0300

    KS 7-5 v.3 added required characters to the string

--- src/main/kotlin/lesson7/lesson7-task5.kt
+++ src/main/kotlin/lesson7/lesson7-task5.kt
@@ -3,20 +3,29 @@ import kotlin.random.Random
 fun main() {
 
     val minPasswordLength = 6
-    var listChars: List<Char>
-    var password: String
+    var password: MutableList<Char> = mutableListOf()
     var passwordLength: Int
+    val digits = '0'..'9'
+    val lowercaseLetters = 'a'..'z'
+    val uppercaseLetters = 'A'..'Z'
+    var allChars: List<Char>
+    password.add(digits.random())
+    password.add(lowercaseLetters.random())
+    password.add(uppercaseLetters.random())
+    val passwordRequiredChar = password.size
+    var listRandomChar: List<Char>
 
     do {
         println("Задайте количество символов в пароле:")
         passwordLength = readln().toInt()
         if (passwordLength >= minPasswordLength) {
-            listChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
-            password = (1..passwordLength).map { listChars.random() }.joinToString("")
-            println(password)
+            allChars = (uppercaseLetters) + (lowercaseLetters) + (digits)
+            listRandomChar = (1..(passwordLength - passwordRequiredChar)).map { allChars.random() }
+            password = password.plus(listRandomChar).toMutableList()
+            password.shuffle()
+            println(password.joinToString(""))
         } else {
             println("Символов должно быть не меньше $minPasswordLength")
         }
     } while (passwordLength < minPasswordLength)
-
 }
