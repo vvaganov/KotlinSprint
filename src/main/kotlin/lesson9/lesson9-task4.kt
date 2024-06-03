8fff96cc3e0974a12250a7433e3d1e9b68ca2514
Valery <i@vvaganov.ru>
2024-06-03 13:33:18 +0300

    KS 9,4 v. 2 single line output

--- src/main/kotlin/lesson9/lesson9-task4.kt
+++ src/main/kotlin/lesson9/lesson9-task4.kt
@@ -2,8 +2,6 @@ package lesson9
 
 fun main() {
     println("Введите 5 ингридиентов через \", \" ")
-    val ingredients = readln()
-    val ingredientsList = ingredients.split(", ")
-    val ingredientsListSorted = ingredientsList.sorted()
-    println(ingredientsListSorted)
+    val ingredients = readln().split(", ").sorted()
+    println(ingredients)
 }
