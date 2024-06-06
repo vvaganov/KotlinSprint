90a4917481af07105e4525b232b8102a339eca5e
Valery <i@vvaganov.ru>
2024-06-06 13:29:29 +0300

    KS 13-2 v. 1

---
+++ src/main/kotlin/lesson13/lesson13-task2.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val contact = PhoneList("Василий", 79022783658)
+    contact.printContact()
+}
+
+class PhoneList(
+    val name: String,
+    val phoneNumber: Long,
+    var company: String? = null
+) {
+    fun printContact() {
+        company = company?: "не указано"
+        println("- Имя: $name\n- Номер: $phoneNumber\n- Компания: $company ")
+    }
+}
