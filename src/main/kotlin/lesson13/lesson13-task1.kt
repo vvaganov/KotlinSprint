44203e432122b48406b3c17210651b624f27ad6b
vvaganov <i@vvaganov.ru>
2024-06-09 09:04:13 +0300

    Delete src/main/kotlin/lesson13/lesson13-task1.kt

--- src/main/kotlin/lesson13/lesson13-task1.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val contact = PhoneDirectory("Василий", 79022783658)
-    }
-
-class PhoneDirectory(
-    val name: String,
-    val phoneNumber: Long,
-    val company: String? = null,
-) {}
