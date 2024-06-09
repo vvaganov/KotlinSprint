e34aa0a6d9ddb4a90b119cbd19998295bf42db10
Valery <i@vvaganov.ru>
2024-06-09 09:42:58 +0300

    KS 13-3 v. 2

--- src/main/kotlin/lesson13/lesson13-task3.kt
+++ src/main/kotlin/lesson13/lesson13-task3.kt
@@ -1,12 +1,16 @@
+package lesson13
+
 fun main() {
     val contactList: MutableList<PhoneBook> = mutableListOf(
         PhoneBook("Василий", 89022783658),
         PhoneBook("Иван", 89275685425),
         PhoneBook("Евгений", 89996931476, "null"),
         PhoneBook("Анатолий", 89537648231, "Камаз"),
-        PhoneBook("Михаил", 79097596452, "Газпром")
+        PhoneBook("Михаил", 79097596452, "Газпром"),
     )
-    contactList.forEach { it.printContact() }
+
+    contactList.forEach { it.printCompany() }
+
 }
 
 class PhoneBook(
@@ -14,9 +18,16 @@ class PhoneBook(
     val phoneNumber: Long,
     var company: String? = null
 ) {
+
     fun printContact() {
         company = company ?: "не указано"
         println("- Имя: $name\n- Номер: $phoneNumber\n- Компания: $company ")
         println("_______________________")
+    }
+
+    fun printCompany() {
+        if (company != null) {
+            println(company)
+        }
     }
 }
