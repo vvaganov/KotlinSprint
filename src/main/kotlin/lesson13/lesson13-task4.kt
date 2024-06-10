1cb6136a2f78965dc4891211ac74c70398f23827
Valery <i@vvaganov.ru>
2024-06-10 18:11:33 +0300

    KS 13-4 v.2.

--- src/main/kotlin/lesson13/lesson13-task4.kt
+++ src/main/kotlin/lesson13/lesson13-task4.kt
@@ -1,20 +1,23 @@
 fun main() {
+    var count = 3
 
     val contactList: MutableList<MyPhoneNumber> = mutableListOf()
 
-    println("Введите имя контакта:")
-    val name = readln()
-    println("Введите номер телефона:")
-    val phoneNumber = readln().toLongOrNull()
-    println("Введите компанию:")
-    val company = readln()
-    if (phoneNumber == null) {
-        println("Вы не ввели номер телефона")
-    } else {
-        contactList.add(MyPhoneNumber(name, phoneNumber, company))
+    while (count > 0) {
+        println("Введите имя контакта:")
+        val name = readln()
+        println("Введите компанию:")
+        val company = readln()
+        println("Введите номер телефона:")
+        val phoneNumber = readln().toLongOrNull()
+        if (phoneNumber == null) {
+            println("Вы не ввели номер телефона")
+        } else {
+            contactList.add(MyPhoneNumber(name, phoneNumber, company))
+        }
+        count--
     }
     contactList.forEach { it.printContact() }
-
 }
 
 class MyPhoneNumber(
