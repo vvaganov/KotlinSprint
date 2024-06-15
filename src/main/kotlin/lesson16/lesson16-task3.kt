3f91c47f1f67a433d38d3c57463314d5b696cef8
Valery <i@vvaganov.ru>
2024-06-15 15:11:09 +0300

    KS 16-3

--- src/main/kotlin/lesson16/lesson16-task3.kt
+++ src/main/kotlin/lesson16/lesson16-task3.kt
@@ -1,21 +1,17 @@
 package lesson16
 
 fun main() {
+    val user = User("admin", "qwerty")
     val password = readln()
-    val user = Users()
     println(user.checkPassword(password))
 
 }
 
-class Users {
-    val login = "login"
-    private val password = "password"
+class User(
+    val login: String,
+    private val password:String,
+    ) {
 
-    fun checkPassword(pass: String): Boolean {
-        if (pass == password) {
-            return true
-        } else {
-            return false
-        }
+    fun checkPassword(pass: String): Boolean = pass == password
     }
-}
+
