8823c669d9c6da100de2ceefa5136e6c0a3dcc01
Valery <i@vvaganov.ru>
2024-06-21 15:18:16 +0300

    KS 16-5 v-1

---
+++ src/main/kotlin/lesson16/lesson16-task5.kt
@@ -0,0 +1,47 @@
+fun main() {
+    val gamer = Gamer("Bob", 6, 3)
+    println( gamer.getHealth())
+    gamer.takeDamage(3)
+    println(gamer.getHealth())
+    gamer.restoreHealth()
+    println(gamer.getHealth())
+    gamer.takeDamage(3)
+    println(gamer.getHealth())
+    gamer.restoreHealth()
+    println(gamer.getHealth())
+    gamer.takeDamage(3)
+    gamer.restoreHealth()
+
+
+}
+
+class Gamer(
+    val name: String = "",
+    private var health: Int,
+    private var impactForce: Int,
+
+) {
+
+    private var isRestoreHealth = true
+
+    fun getHealth() = health
+
+    fun restoreHealth() {
+        if (isRestoreHealth) {
+            health++
+        }
+    }
+
+    fun takeDamage(damage: Int) {
+        health -= damage
+        if (health <= 0){
+            death()
+        }
+    }
+
+    private fun death() {
+        impactForce = 0
+        isRestoreHealth = false
+        println("$name умер: сила удара равна $impactForce, возможность лечиться $isRestoreHealth")
+    }
+}
