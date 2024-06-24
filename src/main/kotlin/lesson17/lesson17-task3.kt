e157c0ab50f4fc7ef2be01522887427024df576b
Valery <i@vvaganov.ru>
2024-06-24 22:17:49 +0300

    KS-17-3 set,get if, else

---
+++ src/main/kotlin/lesson17/lesson17-task3.kt
@@ -0,0 +1,20 @@
+package lesson17
+
+fun main() {
+    val folder = Folder()
+    println( folder.name)
+
+}
+
+class Folder{
+
+    val name: String = "Projects"
+        get() = if (isSecret) {
+            "Скрытая папка, количесто файов $numberFiles"
+        } else "Папка $field, количество файлов $numberFiles "
+
+    private val numberFiles: Int = 20
+        get() = if (isSecret) 0 else field
+
+    private val isSecret = true
+}
