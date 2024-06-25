5bada7168ff10594b0f5a84e3bf3a746ecd7f50a
Valery <i@vvaganov.ru>
2024-06-25 09:59:19 +0300

    KS-15-5

--- src/main/kotlin/lesson15/lesson15-task5.kt
+++ src/main/kotlin/lesson15/lesson15-task5.kt
@@ -3,14 +3,14 @@ package lesson15
 fun main() {
 
     val truck = Track()
-    truck.boardingPassengers(1)
-    truck.loadingCargo(2000)
+    truck.boardPassengers(1)
+    truck.loadCargo(2000)
     truck.move()
     val pass = PassengerCar()
-    pass.boardingPassengers(3)
-    pass.boardingPassengers(2)
+    pass.boardPassengers(3)
+    pass.boardPassengers(2)
     pass.move()
-    println("Перевезено ${Count.pass} пассажиров и ${Count.cargo} груза")
+    println("Перевезено ${pass.pass + truck.pass} пассажиров и ${truck.cargo} груза")
 
 
 }
@@ -20,40 +20,41 @@ interface Movable {
 }
 
 interface Truckable {
-    fun loadingCargo(cargoWeight: Int)
+    fun loadCargo(cargoWeight: Int)
 }
 
 interface Passengerable {
-    fun boardingPassengers(numberPassengers: Int)
+    fun boardPassengers(numberPassengers: Int)
 }
 
 abstract class Transport : Movable, Passengerable {
     abstract val maxPass: Int
     abstract val maxCargo: Int
-}
 
-object Count {
     var pass = 0
     var cargo = 0
 }
 
+
 class Track(override val maxPass: Int = 1, override val maxCargo: Int = 2000) : Transport(), Truckable {
+
+
 
     override fun move() = println("Грузовик начал движение")
 
-    override fun boardingPassengers(numberPassengers: Int) {
+    override fun boardPassengers(numberPassengers: Int) {
         if (numberPassengers > maxPass) {
             println("Максимальное количество пассажиров в грузовике $maxPass пассажир")
         } else {
-            Count.pass += numberPassengers
+            pass += numberPassengers
         }
     }
 
-    override fun loadingCargo(cargoWeight: Int) {
+    override fun loadCargo(cargoWeight: Int) {
         if (cargoWeight > maxCargo) {
             println("Максимальный вес груза в грузовике $maxCargo")
         } else {
-            Count.cargo += cargoWeight
+            cargo += cargoWeight
         }
     }
 }
@@ -61,11 +62,11 @@ class Track(override val maxPass: Int = 1, override val maxCargo: Int = 2000) : 
 class PassengerCar(override val maxPass: Int = 3, override val maxCargo: Int = 0) : Transport() {
     override fun move() = println("Лекговой автомобиль начал движение")
 
-    override fun boardingPassengers(numberPassengers: Int) {
+    override fun boardPassengers(numberPassengers: Int) {
         if (numberPassengers > maxPass) {
             println("Максимальное число пассажиров $maxPass ")
         } else {
-            Count.pass += numberPassengers
+            pass += numberPassengers
         }
     }
 }
