9d660f8cee7f61234fbd5217fd67847a2cc73701
Valery <i@vvaganov.ru>
2024-06-27 14:12:11 +0300

    KS-19-4

---
+++ src/main/kotlin/lesson19/lesson19-task4.kt
@@ -0,0 +1,45 @@
+package lesson19
+
+fun main() {
+
+    val tank = Tank()
+    tank.loadCannon(Patron.GREEN)
+    tank.toFireCannon()
+    tank.loadCannon(Patron.BLUE)
+    tank.toFireCannon()
+    tank.loadCannon(Patron.RED)
+    tank.toFireCannon()
+}
+
+enum class Patron {
+    BLUE {
+        override fun getDamageCaused(): Int {
+            return 5
+        }
+    },
+    GREEN {
+        override fun getDamageCaused(): Int {
+            return 10
+        }
+    },
+    RED {
+        override fun getDamageCaused(): Int {
+            return 20
+        }
+    };
+
+    abstract fun getDamageCaused(): Int
+}
+
+class Tank {
+    private lateinit var patronType: Patron
+
+    fun loadCannon(patron: Patron) {
+        patronType = patron
+        println("Пушка заряжена патроном: ${patron.name}")
+    }
+
+    fun toFireCannon() {
+        println("Выстрел партоном $patronType, нанесенный ущерб - ${patronType.getDamageCaused()} ")
+    }
+}
