1fe1c945097338eb132b5aa48007d99008e90b41
Valery <i@vvaganov.ru>
2024-06-27 10:34:41 +0300

    KS-18-3 add method sleep

--- src/main/kotlin/lesson18/lesson18-task3.kt
+++ src/main/kotlin/lesson18/lesson18-task3.kt
@@ -4,18 +4,23 @@ fun main() {
 
     val animalList = listOf<Animal>(Fox("Алииса"), Dog("Палкан"), Cat("Муська"))
     toFeedAllAnimal(animalList)
-
 }
 
 abstract class Animal {
     abstract val name: String
     abstract fun toEatFood()
+    abstract fun sleep()
 
 }
 
 class Fox(override val name: String) : Animal() {
     override fun toEatFood() {
         println("Лиса $name - ест ягоды")
+
+    }
+
+    override fun sleep() {
+        println("Лиса $name - спит")
     }
 }
 
@@ -23,14 +28,26 @@ class Dog(override val name: String) : Animal() {
     override fun toEatFood() {
         println("Собака $name - ест кости")
     }
+
+    override fun sleep() {
+        println("Собака $name - спит")
+    }
 }
 
 class Cat(override val name: String) : Animal() {
     override fun toEatFood() {
         println("Кошка $name - ест рыбу")
     }
+
+    override fun sleep() {
+        println("Кошка $name - спит")
+    }
 }
 
 fun toFeedAllAnimal(list: List<Animal>) {
     list.forEach { it.toEatFood() }
+}
+
+fun sleepAllAnimal(list: List<Animal>) {
+    list.forEach { it.sleep() }
 }
