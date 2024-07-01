2d60c9e8ef5ccd75748626ba204ab956225d0c3d
Valery <i@vvaganov.ru>
2024-07-01 16:49:56 +0300

    KS-21-3

--- src/main/kotlin/lesson21/lesson21-task3.kt
+++ src/main/kotlin/lesson21/lesson21-task3.kt
@@ -4,11 +4,11 @@ fun main() {
     val player = PlayerOne("Bob", 10)
     println(player.health)
     println(player.maxHealth)
-    println(player.isHealthy(player))
+    println(player.isHealthy())
     restoreHealth(player)
     println(player.health)
     println(player.maxHealth)
-    println(player.isHealthy(player))
+    println(player.isHealthy())
 
 }
 
@@ -21,6 +21,6 @@ class PlayerOne(
 
 val restoreHealth: (PlayerOne) -> Unit = { player: PlayerOne -> player.health = player.maxHealth }
 
-fun PlayerOne.isHealthy(player: PlayerOne): Boolean {
-    return player.health == player.maxHealth
+fun PlayerOne.isHealthy(): Boolean {
+    return this.health == this.maxHealth
 }
