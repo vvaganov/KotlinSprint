71b92dd698abb54171ccd9af3f9e2510e4e22298
Valery <i@vvaganov.ru>
2024-07-01 10:24:56 +0300

    KS-20-4 forEachIndex, autoformat

--- src/main/kotlin/lesson20/lesson20-task4.kt
+++ src/main/kotlin/lesson20/lesson20-task4.kt
@@ -1,9 +1,12 @@
 package lesson20
 
 fun main() {
-    val elementList: List<String> = listOf("element1", "element2", "element3", "element4", "element5", "element6", "element7")
-    val lambdaList = (elementList.map { { println("Нажат элемент - $it") } }).withIndex()
-    lambdaList.forEach { if ((it.index +1) % 2 == 0)
-        it.value()
+    val elementList: List<String> =
+        listOf("element1", "element2", "element3", "element4", "element5", "element6", "element7")
+    val lambdaList = elementList.map { { println("Нажат элемент - $it") } }
+    lambdaList.forEachIndexed { index, function ->
+        if ((index + 1) % 2 == 0) {
+            function()
+        }
     }
 }
