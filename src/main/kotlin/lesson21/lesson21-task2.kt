8dce07a2b01b4bf7ddaa338d34f739d5892d0222
Valery <i@vvaganov.ru>
2024-07-01 16:47:19 +0300

    KS-21-2

--- src/main/kotlin/lesson21/lesson21-task2.kt
+++ src/main/kotlin/lesson21/lesson21-task2.kt
@@ -2,9 +2,9 @@ package lesson21
 
 fun main() {
     val numberList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    println(numberList.evenNumbersSum(numberList))
+    println(numberList.evenNumbersSum())
 }
 
-fun List<Int>.evenNumbersSum(numberList: List<Int>): Int {
-    return numberList.filter { it % 2 == 0 }.sum()
+fun List<Int>.evenNumbersSum(): Int {
+    return this.filter { it % 2 == 0 }.sum()
 }
