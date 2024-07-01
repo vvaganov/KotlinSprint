96bd4744db8fb087ddaa66a43902495f9616ada7
Valery <i@vvaganov.ru>
2024-07-01 09:23:45 +0300

    KS-20-1 create lambda functions

--- src/main/kotlin/lesson20/lesson20-task1.kt
+++ src/main/kotlin/lesson20/lesson20-task1.kt
@@ -2,7 +2,8 @@ package lesson20
 
 fun main() {
 
-    val userName = "Василий"
-
-    { name: String -> println("С наступающим Новым Годом, $name!") }(userName)
+    val userName = "Гена"
+    congratulate(userName)
 }
+
+val congratulate: (String) -> Unit = { println("С наступающим Новым Годом, $it!") }
