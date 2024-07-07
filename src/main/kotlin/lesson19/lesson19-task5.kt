67fa0329cc278818f37097585c2319966198e68a
Valery <i@vvaganov.ru>
2024-07-07 09:40:59 +0300

    KS-19-5 checking gender in a loop

--- src/main/kotlin/lesson19/lesson19-task5.kt
+++ src/main/kotlin/lesson19/lesson19-task5.kt
@@ -1,6 +1,6 @@
 package lesson19
 
-const val NUMBER_PERSON = 5
+const val NUMBER_PERSON = 1
 
 fun main() {
     val humanList: MutableList<Human> = mutableListOf<Human>()
@@ -11,13 +11,13 @@ fun main() {
         humanList.add(Human(inputName, getGender()))
     }
 
-    humanList.forEach { println("Имя ${it.name} -  пол: ${(it.gender?.textName)}") }
+    humanList.forEach { println("Имя ${it.name} -  пол: ${(it.gender.textName)}") }
 
 }
 
 class Human(
     val name: String,
-    val gender: Gender?,
+    val gender: Gender,
 )
 
 enum class Gender(val textName: String) {
@@ -25,20 +25,29 @@ enum class Gender(val textName: String) {
     WOMEN("Женский");
 }
 
+fun getName() {
+    val name: String
+
+}
+
 fun getGender(): Gender {
     val gender: Gender
     println("Введите пол:")
     while (true) {
         Gender.entries.forEach { println("- ${it.textName}") }
-        val asd = readln()
-        if (asd == Gender.MEN.textName) {
-            gender = Gender.MEN
-            break
-        } else if (asd == Gender.WOMEN.textName) {
-            gender = Gender.WOMEN
-            break
-        } else {
-            println("Введите корректный пол:")
+        val inputGender = readln()
+        when (inputGender) {
+            Gender.WOMEN.textName -> {
+                gender = Gender.WOMEN
+                break
+            }
+
+            Gender.MEN.textName -> {
+                gender = Gender.MEN
+                break
+            }
+
+            else -> println("Введите корректный пол:")
         }
     }
     return gender
